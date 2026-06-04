fa01589667c842c951dcf439e261fff2a10fe99a
putassu <57058602+putassu@users.noreply.github.com>
2026-06-04 10:09:41 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Text, JSON, Boolean
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
-engine = create_engine("sqlite:///dataset.db", connect_args={"check_same_thread": False})
+engine = create_engine("sqlite:////app/server_data/dataset.db", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
